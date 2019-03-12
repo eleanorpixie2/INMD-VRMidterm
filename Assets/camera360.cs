a452e51436ad6194027a464dead6ddf50ccb6b40
Tess Olson <37564977+eleanorpixie2@users.noreply.github.com>
2019-03-12 14:20:26 -0400

    Added 360 camera renderer

--- Assets/camera360.cs
+++ Assets/camera360.cs
@@ -1,23 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Rendering;
 
+//attach this script to your camera object
 public class camera360 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public RenderTexture cubemapLeft;
+    public RenderTexture cubemapRight;
+    public RenderTexture equirect;
+    public bool renderStereo = true;
+    public float stereoSeparation = 0.064f;
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        Camera cam = GetComponent<Camera>();
+
+        if (cam == null)
         {
-            Debug.Log("Run1");
-            File.WriteAllBytes(@"C:", I360Render.Capture());
+            cam = GetComponentInParent<Camera>();
+        }
+
+        if (cam == null)
+        {
+            Debug.Log("stereo 360 capture node has no camera or parent camera");
+        }
+
+        if (renderStereo)
+        {
+            cam.stereoSeparation = stereoSeparation;
+            cam.RenderToCubemap(cubemapLeft, 63, Camera.MonoOrStereoscopicEye.Left);
+            cam.RenderToCubemap(cubemapRight, 63, Camera.MonoOrStereoscopicEye.Right);
+        }
+        else
+        {
+            cam.RenderToCubemap(cubemapLeft, 63, Camera.MonoOrStereoscopicEye.Mono);
+        }
+
+        //optional: convert cubemaps to equirect
+
+        if (equirect == null)
+        {
+            return;
+        }
+
+        if (renderStereo)
+        {
+            cubemapLeft.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Left);
+            cubemapRight.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Right);
+        }
+        else
+        {
+            cubemapLeft.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Mono);
         }
     }
 }
